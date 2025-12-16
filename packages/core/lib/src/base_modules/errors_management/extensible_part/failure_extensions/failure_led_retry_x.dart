d8a499f9b389bb33e47ad300431d860af93506f9
Roman Godun <4l.roman.godun@gmail.com>
2025-12-16 11:57:01 +0200

    refactor: refactor failure_led_retry_x.dart

--- packages/core/lib/src/base_modules/errors_management/extensible_part/failure_extensions/failure_led_retry_x.dart
+++ packages/core/lib/src/base_modules/errors_management/extensible_part/failure_extensions/failure_led_retry_x.dart
@@ -1,11 +1,11 @@
-import 'package:core/src/base_modules/errors_management/core_of_module/failure_entity.dart';
-import 'package:core/src/base_modules/errors_management/core_of_module/failure_type.dart';
+import 'package:core/public_api/base_modules/errors_management.dart';
 
 /// 🧠 [FailureRetryX] — extension that defines whether [Failure] is retryable
 /// ✅ Used by UI logic to decide if "Retry" should be shown to the user
 //
 extension FailureRetryX on Failure {
   ///
+  /// 🔁 True if failure is transient and safe to retry
   bool get isRetryable => type.isRetryable;
 }
 
@@ -15,10 +15,8 @@ extension FailureRetryX on Failure {
 /// ✅ Allows UI to decide if retry button should be shown
 //
 extension FailureTypeRetryX on FailureType {
+  ///
   /// 🔁 True if the failure is transient and can be retried by user
-  bool get isRetryable {
-    if (this is NetworkFailureType) return true;
-    if (this is NetworkTimeoutFailureType) return true;
-    return false;
-  }
+  bool get isRetryable =>
+      code == FailureCodes.network || code == FailureCodes.timeout;
 }
